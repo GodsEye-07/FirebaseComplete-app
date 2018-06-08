ca27e5612fa1efee2e2697084b99c44d3413832e
Ayush Verma <vermaayush.verma76@gmail.com>
2018-06-08 23:51:43 +0530

    chatVC added.

---
+++ FirebaseComplete/ViewControllers/CallViewController/CallTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CallTableViewCell.swift
+//  FirebaseComplete
+//
+//  Created by Ayush Verma on 08/06/18.
+//  Copyright © 2018 Ayush Verma. All rights reserved.
+//
+
+import UIKit
+
+class CallTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FirebaseComplete/ViewControllers/CallViewController/CallViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CallViewController.swift
+//  FirebaseComplete
+//
+//  Created by Ayush Verma on 08/06/18.
+//  Copyright © 2018 Ayush Verma. All rights reserved.
+//
+
+import UIKit
+
+class CallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
